07b6f449aeec03575c22013834a23419922aa148
adebayo <kurdzsamuel@gmail.com>
2021-03-20 18:22:59 +0100

    validate eligibilty form

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -11,7 +11,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class ModalComponent implements OnInit {
   @Output() closeModal = new EventEmitter<boolean>();
 
-  displayedFormIndex = 1;
+  displayedFormIndex = 0;
   eligibilityForm: FormGroup;
   personalForm: FormGroup;
   employerForm: FormGroup;
@@ -71,8 +71,34 @@ export class ModalComponent implements OnInit {
     this.closeModal.emit(false);
   }
 
-  submitGenericForm(): void {
+  startApplication(): void {
     this.displayedFormIndex += 1;
+  }
+
+  submitGenericForm(formName: string): void {
+    switch (formName) {
+      case 'personalForm':
+        if (this.personalForm.invalid) {
+          return;
+        } else {
+          this.displayedFormIndex += 1;
+        }
+        break;
+      case 'employerForm':
+        if (this.employerForm.invalid) {
+          return;
+        } else {
+          this.displayedFormIndex += 1;
+        }
+        break;
+      default:
+        if (this.eligibilityForm.invalid) {
+          return;
+        } else {
+          this.displayedFormIndex += 1;
+        }
+        break;
+    }
   }
 
 }
